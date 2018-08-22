ca59036ac7cad7dd224ca348142c397598cdafcd
pzombade <prashantzombade@gmail.com>
2018-08-22 18:13:27 +0530

    fixes around port

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
-
-app.listen(25450 || process.env.PORT,function(){
-    console.log("server.js up and running on port "+process.env.PORT);
+var port = process.env.PORT || 3000;
+app.listen(port ,function(){
+    console.log("up and running on port "+port);
 });
+
 
 app.post('/webhook', function (req, res) {
   res.end("server.js Post Hellow from webhook");
