e2edbc85843ab9b6ecf0e0f95a73aee8b92fa0e0
pzombade <prashantzombade@gmail.com>
2018-08-22 17:33:37 +0530

    fixed port issues

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.listen(3000 || process.env.PORT,function(){
+    console.log("index.js up and running on port "+process.env.PORT);
+});
 
 app.post('/webhook', function (req, res) {
   res.end("index.js Post Hellow from webhook");
@@ -10,11 +13,3 @@ app.get('/webhook', function (req, res) {
   res.end("index.js Get Hellow from webhook");
 });
 
-var server = app.listen(5000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  //console.log("Example app listening at http://%s:%s", host, port)
-
-})
